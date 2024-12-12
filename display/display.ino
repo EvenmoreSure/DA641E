e7689983475bd938e15b7295f2702cf753b3ef13
C3dvall <96820963+C3dvall@users.noreply.github.com>
2024-12-12 13:17:30 +0100

    Update display.ino
    Removed the constructor which didn't work.

--- display/display.ino
+++ display/display.ino
@@ -5,7 +5,6 @@
 
 // display connected to the HW I2C (this is the best option)
 U8G2_SH1107_SEEED_128X128_F_HW_I2C u8g2(U8G2_R0, /* reset=*/U8X8_PIN_NONE);
-//U8G2_SH1107_SEEED_128X128_F_SW_I2C u8g2(U8G2_R0, /* clock SCL GUL=*/ 19, /* data SDA VIT=*/ 18, /* reset=*/ U8X8_PIN_NONE);
 
 void setup(void) {
   u8g2.begin();
