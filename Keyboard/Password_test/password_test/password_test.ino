1ee9dfff5f07d4cd8dd25830d0c490b686e282f4
Linnea Dahlgren <dahlgren99@gmail.com>
2024-12-12 12:33:43 +0100

    password check

---
+++ Keyboard/Password_test/password_test/password_test.ino
@@ -0,0 +1,53 @@
+#include <Keypad.h>
+
+const byte ROWS = 4; 
+const byte COLS = 3; 
+
+char hexaKeys[ROWS][COLS] = {
+  {'1', '2', '3'},
+  {'4', '5', '6'},
+  {'7', '8', '9'},
+  {'*', '0', '#'}
+};
+
+// pins for the black keyboard
+byte rowPins[ROWS] = {5, 6, 7, 8}; //connect to the row pinouts of the keypad
+byte colPins[COLS] = {2, 3, 4}; //connect to the column pinouts of the keypad
+
+Keypad keypad = Keypad(makeKeymap(hexaKeys), rowPins, colPins, ROWS, COLS); 
+
+
+const String password = "123"; // change your password here
+String input_password;
+
+void setup() {
+  Serial.begin(9600);
+  input_password.reserve(32); // maximum input characters is 33, change if needed
+}
+
+void loop() {
+  char key = keypad.getKey();
+
+  if (key) {
+    Serial.println(key);
+    input_password += key; // append new character to input password string
+
+  //  if (key == '*') {
+  //    input_password = ""; // clear input password
+    //} else
+     if (input_password.length() == password.length()) {
+      if (password == input_password) {
+        Serial.println("password is correct");
+        // DO YOUR WORK HERE
+
+      } else {
+        Serial.println("password is incorrect, try again");
+
+      }
+        input_password = ""; // clear input password
+
+    //} else {
+    }
+  }
+  delay(100);
+}
